1249801582c6fdb3af962519457543e29de25e40
sanjayshankar <sanjayninjas6@gmail.com>
2024-06-27 21:38:02 +0530

    First commit

---
+++ Rough.c
@@ -0,0 +1,15 @@
+  #include <stdio.h>
+  #include <stdlib.h>
+
+  int main(int argc, char const *argv[]) {
+
+  int a;
+  int b;
+  int sum;
+  printf("Enter two numbersk you want to sum:");
+  scanf("%d%d",&a, &b);
+  sum = a+b;
+  printf("sum: %d", sum);
+  printf("\n");
+  return 0;
+  }
